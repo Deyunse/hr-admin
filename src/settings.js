4e8488b0cdecdda35015e2305fbb5ad07a7c984b
deyun <941520431@qq.com>
2022-07-13 11:52:26 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '德云社人力资源',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   *  设置Main右侧主题上面的header是不是固定位
+    */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 是否显示左边侧边栏 上的log 是否显示
+   */
+  sidebarLogo: true
+}
